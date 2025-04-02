573ffae18d85b074927796fd28e4cb7365044396
F-iiral <tylerwolff05@gmail.com>
2025-04-02 19:27:33 +0200

    Made a script load common elements

---
+++ scripts/loadCommon.js
@@ -0,0 +1,17 @@
+function main() {
+    console.log("Loading common HTML elements")
+
+    var topbar = document.getElementById("topbar");
+    topbar.setHTMLUnsafe( 
+    '<h1>Project Keen Sword</h1>' +
+    '<ul class="topbar-selector>' +
+    '    <li class="topbar-selector-item"><a href="/">Home</a></li>' +
+    '    <li class="topbar-selector-item"><a href="/blog">Blog</a></li>' +
+    '    <li class="topbar-selector-item"><a href="/gallery">Gallery</a></li>' +
+    '    <li class="topbar-selector-item"><a href="/contact">Contact</a></li>' +
+    '    <li class="topbar-selector-item"><a href="/characters">Characters</a></li>' +
+    '    <li class="topbar-selector-item"><a href="/nations">Nations</a></li>' +
+    '</ul>');
+}
+
+main();
