fd96599cd93d85edb947b7a4178ac740a24f3d68
F-iiral <tylerwolff05@gmail.com>
2025-04-12 17:30:44 +0200

    Added Gallery

---
+++ scripts/gallery.js
@@ -0,0 +1,57 @@
+function lazyLoadImages() {
+    const images = document.querySelectorAll('img.lazy');
+
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting)
+                return;
+
+            const img = entry.target;
+            const wrapper = img.closest('.gallery-wrapper');
+
+            img.src = img.dataset.src;
+            img.onload = () => {
+                img.style.opacity = 1;
+                const spinner = wrapper.querySelector('.spinner');
+                if (spinner) spinner.remove();
+            };
+
+            img.classList.remove("lazy");
+            observer.unobserve(img);
+        });
+    });
+
+    images.forEach(img => {
+        imageObserver.observe(img);
+    });
+}
+
+function addSearchBar() {
+    document.getElementById('search-input').addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase().trim();
+        const searchTerms = searchTerm.split(';').map(term => term.trim());
+        const galleryItems = document.querySelectorAll('.gallery-wrapper');
+    
+        galleryItems.forEach(item => {
+            const caption = item.querySelector('p').textContent.toLowerCase();
+            const alt = item.querySelector('img').alt.toLowerCase();
+            const keywords = item.querySelector('img').dataset.keywords.toLowerCase();
+            
+            const matchesSearchTerms = searchTerms.some(term => 
+                caption.includes(term) || alt.includes(term) || keywords.includes(term)
+            );
+
+            if (matchesSearchTerms) {
+                item.style.display = '';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    addSearchBar();
+    lazyLoadImages();
+});
